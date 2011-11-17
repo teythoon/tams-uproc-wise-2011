1e94fe15eaac8f5bea8997cff82d9a1f3429e026
Claudius <claudius@ambtec.de>
2011-11-17 17:41:44 +0100

    little changes

--- src/alu.vhdl
+++ src/alu.vhdl
@@ -1,16 +1,22 @@
+-- Todo: add missing alu operations and encode opcodes
+
 if(rising_edge(Clk)) then
   case OpCode is
-    when "000" =>
+    when "" =>
       Reg1 = Reg1 + Reg2;
-    when "001" =>
+    when "" =>
       Reg1 = Reg1 - Reg2;
-    when "010" =>
+    when "" =>
+      Reg1 = Reg1 * Reg2;
+    when "" =>
+      Reg1 = Reg1 / Reg2;
+    when "" =>
       Reg1 = Reg1 and Reg2;
-    when "011" => 
+    when "" => 
       Reg1 = Reg1 or Reg2;
-    when "100" => 
+    when "" => 
       Reg1 = Reg1 xor Reg2;
-    when "101" =>
+    when "" =>
       Reg1 = not Reg1;
     when others =>
       NULL;
