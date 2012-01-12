23b585a8107ec6a50fa03a38d52cb96915096f61
Justus Winter <4winter@informatik.uni-hamburg.de>
2012-01-12 16:42:14 +0100

    fix whitespace

--- src/registers.py
+++ src/registers.py
@@ -36,12 +36,11 @@ def RegisterBank(
     '''
 
     registers = [Signal(data_bus(0)) for i in range(number_of_registers)]
-    
+
     @always_comb
     def read_logic():
         value_a.next = registers[select_a]
         value_b.next = registers[select_b]
-    
 
     @always(clock.posedge)
     def logic():
