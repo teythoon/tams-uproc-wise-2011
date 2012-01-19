053170c664c30ff707e3c71115b9a00c7c7ff6c1
Claudius <claudius@ambtec.de>
2012-01-19 18:00:51 +0100

    build ram testbench

--- src/ram.py
+++ src/ram.py
@@ -37,6 +37,56 @@ def Ram(clock, data_out, data_in, address, write_enabled, number_of_ram=128):
 
     return write, read
 
+def bench(number_of_ram=128):
+
+    address = Signal(intbv(min = 0, max = number_of_ram))
+    data_in = Signal(data_bus(0))
+    data_out = Signal(data_bus(0))
+    write_enabled = Signal(False)
+    clock = Signal(False)
+
+    ram =  Ram(
+        clock,
+        data_out, data_in,
+        address,
+        write_enabled,
+        number_of_ram,
+    )
+
+    @instance
+    def stimulus():
+        for i in range(number_of_ram):
+            for v in range(100):
+
+                write_enabled.next = True
+                address.next = i
+                data_in.next = v
+
+                clock.next = True
+                yield delay(10)
+                clock.next = False
+                yield delay(10)
+
+                write_enabled.next = False 
+                address.next = i
+
+                clock.next = True
+                yield delay(10)
+                clock.next = False
+                yield delay(10)
+ 
+                result = data_out.next 
+
+                assert result.signed() == v
+
+    return ram, stimulus
+
+def test_bench():
+    sim = Simulation(traceSignals(bench))
+    sim.run()
+
+
+
 if __name__ == '__main__':
     from myhdl import toVHDL
 
